1b9a371a6a0898a4aa3388e09abc031e00df6b14
Daniel Nordstrom <d@mrnordstrom.com>
2019-09-19 03:52:40 +0200

    idk.

--- use_dark_theme.sh
+++ use_dark_theme.sh
@@ -8,21 +8,17 @@ KVANTUM_THEME="AyuMirage"
 SCRIPT_PATH=$(dirname "$(realpath -s "$0")")
 
 # Set GTK3+ theme
-
 gsettings set $GTK_SCHEMA gtk-theme "$GTK_THEME"
 gsettings set $GTK_SCHEMA icon-theme "$GTK_ICON_THEME"
 gsettings set $GTK_SCHEMA font-name "$FONT"
 gsettings set $GTK_SCHEMA document-font-name "$FONT"
 
 # Set KDE/Kvantum theme
-
 kvantummanager --set $KVANTUM_THEME
 
 # .Xdefaults (urxvt does not read .Xresources on launch)
-
 ln -sf "$SCRIPT_PATH/Xresources/.Xresources.ayu-mirage" "$HOME/.Xdefaults"
 
 # Termite
-
 mkdir -p "$HOME/.config/termite"
 ln -sf "$SCRIPT_PATH/termite/config.ayu-mirage" "$HOME/.config/termite/config"
--- use_light_theme.sh
+++ use_light_theme.sh
@@ -3,26 +3,22 @@
 FONT="SF Pro Text Regular 9"
 GTK_THEME="Ayu"
 GTK_ICON_THEME="Papirus"
-KVANTUM_THEME="Ayu"
 GTK_SCHEMA="org.gnome.desktop.interface"
+KVANTUM_THEME="Ayu"
 SCRIPT_PATH=$(dirname "$(realpath -s "$0")")
 
 # Set GTK3+ theme
-
 gsettings set $GTK_SCHEMA gtk-theme "$GTK_THEME"
 gsettings set $GTK_SCHEMA icon-theme "$GTK_ICON_THEME"
 gsettings set $GTK_SCHEMA font-name "$FONT"
 gsettings set $GTK_SCHEMA document-font-name "$FONT"
 
 # Set KDE/Kvantum theme
-
 kvantummanager --set $KVANTUM_THEME
 
 # Copy and overwrite .Xdefaults
-
 ln -sf "$SCRIPT_PATH/Xresources/.Xresources.ayu-reduced" "$HOME/.Xdefaults"
 
 # Termite
-
 mkdir -p "$HOME/.config/termite"
 ln -sf "$SCRIPT_PATH/termite/config.ayu-reduced" "$HOME/.config/termite/config"
