27fdbf4743c3be61954b9ff1630724faa0998f05
Daniel Nordstrom <d@mrnordstrom.com>
2019-09-22 19:18:22 +0200

    Added Kvantum theme.

---
+++ kvantum/install.sh
@@ -0,0 +1,5 @@
+SCRIPT_PATH=$(dirname "$(realpath -s "$0")")
+
+ln -sf "$SCRIPT_PATH/Ayu" /usr/share/Kvantum/
+ln -sf "$SCRIPT_PATH/AyuDark" /usr/share/Kvantum/
+ln -sf "$SCRIPT_PATH/AyuMirage" /usr/share/Kvantum/
