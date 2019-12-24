4c5a9758a0434f226dd59b3d8933047c44867be4
Daniel Nordstrom <d@mrnordstrom.com>
2019-12-24 19:32:25 +0100

    Fixing variable name casing.

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -12,7 +12,7 @@ SCRIPT_PATH=$(dirname "$(realpath -s "$0")")
 ENV_FILE="$SCRIPT_PATH/.env"
 
 # Load environment file
-if [ -f "$ENV_FILE" ]; then
+if [[ -f "$ENV_FILE" ]]; then
   source "$ENV_FILE"
 else
   echo "Missing API key"
@@ -20,45 +20,45 @@ else
 fi
 
 # Location (`units=us` for Fahrenheit)
-location="https://api.darksky.net/forecast/${DOTSIES_WEATHER_API_KEY}/62.3908,17.3069?units=si&exclude=minutely,hourly,daily,alerts,flags"
+LOCATION="https://api.darksky.net/forecast/${DOTSIES_WEATHER_API_KEY}/62.3908,17.3069?units=si&exclude=minutely,hourly,daily,alerts,flags"
 
 # Get weather
-weather="$(curl -s $location)"
+WEATHER="$(curl -s $LOCATION)"
 
 # Get condition
-[[ "$weather" =~ \"icon\":\"([^\"]*)\" ]]
-condition="${BASH_REMATCH[1]}"
+[[ "$WEATHER" =~ \"icon\":\"([^\"]*)\" ]]
+CONDITION="${BASH_REMATCH[1]}"
 
 # Get apparent temperature (`temperature` for actual temperature)
-[[ "$weather" =~ \"apparentTemperature\":([^,]*), ]]
-temperature="${BASH_REMATCH[1]}"
+[[ "$WEATHER" =~ \"apparentTemperature\":([^,]*), ]]
+TEMPERATURE="${BASH_REMATCH[1]}"
 
 # Print custom weather string
-if grep -qi 'rain' <<< $condition; then
+if grep -qi 'rain' <<< $CONDITION; then
   printf " Shit weather"
 
-elif grep -qi 'snow' <<< $condition; then
+elif grep -qi 'snow' <<< $CONDITION; then
 	printf " Shit weather"
 
-elif grep -qi 'wind' <<< $condition; then
+elif grep -qi 'wind' <<< $CONDITION; then
 	printf " Shit weather"
 
-elif grep -qi 'sleet' <<< $condition; then
+elif grep -qi 'sleet' <<< $CONDITION; then
 	printf " Shit weather"
 
-elif grep -qi 'cloudy' <<< $condition; then
+elif grep -qi 'cloudy' <<< $CONDITION; then
   printf " Boring weather"
 
-elif grep -qi 'fog' <<< $condition; then
+elif grep -qi 'fog' <<< $CONDITION; then
 	printf " Boring weather"
 
-elif grep -qi 'partly-cloudy' <<< $condition; then
+elif grep -qi 'partly-cloudy' <<< $CONDITION; then
   printf " Decent weather"
 
-elif grep -qi 'clear-day' <<< $condition; then
+elif grep -qi 'clear-day' <<< $CONDITION; then
   printf " Great weather"
 
-elif grep -qi 'clear-night' <<< $condition; then
+elif grep -qi 'clear-night' <<< $CONDITION; then
 	printf " Great weather"
 
 else
@@ -69,10 +69,10 @@ fi
 printf " "
 
 # Print rounded temperature
-LC_ALL=C /usr/bin/printf '%.0f' " $temperature"
+LC_ALL=C /usr/bin/printf '%.0f' " $TEMPERATURE"
 
 # Print unit character
-if grep -qi 'units=us' <<< $address; then
+if grep -qi 'units=us' <<< $LOCATION; then
 	printf "°F"
 else
 	printf "°C"
