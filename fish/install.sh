da2d529e26c63adbc1481cf451e2a1c52e793dc2
Daniel Nordstrom <d@mrnordstrom.com>
2019-12-25 04:33:05 +0100

    Adding folder and installer for fish shell function files.

--- fish/install.sh
+++ fish/install.sh
@@ -2,6 +2,10 @@
 
 SCRIPT_PATH="$(dirname "$(realpath -s "$0")")"
 
-mkdir -p "$HOME/.config/fish"
+mkdir -p "$HOME/.config/fish/functions"
 
 ln -sf "$SCRIPT_PATH/config.fish" "$HOME/.config/fish/config.fish"
+
+for filename in "$SCRIPT_PATH"/functions/*; do
+  ln -sf "$filename" "$HOME/.config/fish/functions/"
+done
