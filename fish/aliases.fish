cc21555b8fff7e1bd635cf9cd707267ef469cd14
Daniel Nordstrom <d@mrnordstrom.com>
2021-01-05 03:15:49 +0100

    Fixing problems with Screensnap.

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -2,5 +2,4 @@
 # Fish shell aliases basically define functions that run other functions
 #
 
-alias ffs="pls"
 alias sway-windows="swaymsg -t get_tree | jq -r 'recurse(.nodes[]) | .nodes[].window_properties'"
