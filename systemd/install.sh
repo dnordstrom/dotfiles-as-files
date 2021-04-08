950beff4736d3413f540bf6d22c342f9efb85fe0
Daniel Nordstrom <d@mrnordstrom.com>
2021-04-08 22:54:33 +0200

    Using systemd for Sway.

--- systemd/install.sh
+++ systemd/install.sh
@@ -2,8 +2,8 @@
 
 SCRIPT_PATH="$(dirname "$(realpath -s "$0")")"
 
-mkdir -p "$HOME/.config/systemd/"
+mkdir -p "$HOME/.config/systemd/user/"
 
 for filename in "$SCRIPT_PATH"/user/*; do
-  ln -sf "$filename" "$HOME/.config/systemd/"
+  ln -sf "$filename" "$HOME/.config/systemd/user/"
 done
